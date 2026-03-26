dc6fbd57d4aca34db2b4a65c2dcdd44a484557e5
maurice-borys <brillbusmorriss@gmail.com>
2026-03-26 21:19:58 +0000

    added basic sqlite lookup, removed ttl, basic jwt auth token auth, added extractors

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+from fastapi.responses import FileResponse
+from contextlib import asynccontextmanager
+import aiosqlite
 from fastapi import FastAPI
-from starlette.responses import FileResponse
-
 from auth import router as auth_router
 
-ORIGIN: str = "http://127.0.0.1:8000"
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    # Here so a single db connection can be used throughout the server lifetime.
+    application.state.ORIGIN = "http://127.0.0.1:8000"
+    application.state.CAS_ORIGIN = "https://auth.bath.ac.uk"
+    application.state.ALLOWED_ORIGINS = [application.state.ORIGIN] # add frontend dev server here
+    application.state.db = await aiosqlite.connect("test.db")
+    yield
+    await app.state.db.close()
 
+app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
 
 @app.get("/")
