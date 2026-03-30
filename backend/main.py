f0bcaf8d60806cf8f0cad709ef6c420a9242c1ad
maurice-borys <brillbusmorriss@gmail.com>
2026-03-30 17:17:26 +0100

    cleaning merge

--- backend/main.py
+++ backend/main.py
@@ -1,39 +1,27 @@
 from fastapi.responses import FileResponse
-<<<<<<< HEAD
-from auth import router as auth_router
-from fastapi.middleware.cors import CORSMiddleware
-
 import aiosqlite
-from pathlib import Path
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
 from extract import DBNAME
 
-=======
 from fastapi import FastAPI
 from auth import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
 
->>>>>>> 493abd69289deaa3f726bd9aee50dfbbe3d29d8b
 DEV_SERVER = "http://localhost:5173"
 ORIGIN = "http://localhost:8000"
 CAS_ORIGIN = "https://auth.bath.ac.uk"
 
-<<<<<<< HEAD
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
-    import os
-    print(os.getcwd())
-
     try:
-        with open("backend/schema.sql", 'r') as schema_file:
+        with open("backend/sql/schema.sql", 'r') as schema_file:
             schema_sql = schema_file.read()
             async with aiosqlite.connect(DBNAME) as db:
                 await db.executescript(schema_sql)
                 await db.commit()
                 print(f"Schema applied")
     except FileNotFoundError:
-        with open("schema.sql", 'r') as schema_file:
+        with open("sql/schema.sql", 'r') as schema_file:
             schema_sql = schema_file.read()
             async with aiosqlite.connect(DBNAME) as db:
                 await db.executescript(schema_sql)
@@ -43,9 +31,6 @@ async def lifespan(_app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-=======
-app = FastAPI()
->>>>>>> 493abd69289deaa3f726bd9aee50dfbbe3d29d8b
 
 app.add_middleware(
     CORSMiddleware, # type: ignore
@@ -64,9 +49,4 @@ app.include_router(auth_router, prefix="/api")
 
 @app.get("/")
 async def index():
-<<<<<<< HEAD
     return FileResponse("index.html")
-=======
-    return FileResponse("index.html")
-
->>>>>>> 493abd69289deaa3f726bd9aee50dfbbe3d29d8b
