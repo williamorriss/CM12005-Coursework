435281d8fee7d93bb5850b7b9ff46eece026546d
maurice-borys <brillbusmorriss@gmail.com>
2026-04-01 00:49:58 +0100

    sketch of sensor api, needs test page to test endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
 from fastapi.responses import FileResponse
-import aiosqlite
 from contextlib import asynccontextmanager
-from extract import DBNAME
+from db import init_db
 
 from fastapi import FastAPI
 from auth import router as auth_router
+from sensors import router as sensors_router
 from fastapi.middleware.cors import CORSMiddleware
-from physical.testsensor import TestSensor
+
 
 DEV_SERVER = "http://localhost:5173"
 ORIGIN = "http://localhost:8000"
@@ -14,22 +14,8 @@ CAS_ORIGIN = "https://auth.bath.ac.uk"
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
-    test_sensor = TestSensor(plant_id=0)
-    test_sensor.start()
-
     await init_db()
     yield
-
-async def init_db():
-    with open("sql/schema.sql", 'r') as schema_file:
-        schema_sql = schema_file.read()
-        async with aiosqlite.connect(DBNAME) as db:
-            await db.execute("PRAGMA journal_mode=WAL") # Write Ahead Log for concurrent read/ writes
-            await db.execute("PRAGMA busy_timeout=1000")
-            await db.execute("PRAGMA synchronous=NORMAL")
-            await db.executescript(schema_sql)
-            await db.commit()
-            print(f"Schema applied")
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
@@ -45,7 +31,9 @@ app.state.ORIGIN = ORIGIN
 app.state.CAS_ORIGIN = CAS_ORIGIN
 app.state.KEY = "testkeybpfdgsfgoiabegoabgongebiagiaebgaobxxxxxegiobaeogbieaosbget"
 app.state.ALLOWED_ORIGINS = [app.state.ORIGIN, DEV_SERVER]
+app.state.sensors = {}
 app.include_router(auth_router, prefix="/api")
+app.include_router(sensors_router, prefix="/api")
 
 @app.get("/")
 async def index():
