d4446e7b69cda101ff5b13a6a4f9adf3f5d04087
Lav <lavanyabasu031@gmail.com>
2026-04-02 01:30:12 -0400

    removed notes from home page

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,14 +1,10 @@
 import { useAuth, type User } from "../../AuthContext";
 import {type JSX, useEffect} from "react";
-
+import "./Home.css";
 import { Notes } from "../PlantPage/Notes";
 import { useNavigate } from 'react-router-dom'
 
-<<<<<<< HEAD
-function Home() : JSX.Element {
-=======
 export function Home() : JSX.Element {
->>>>>>> 13e1dc504d87b0fa4288ec041312f01c468236f7
     const { session, isLoggedIn, logout, deleteUser, login, getSession } = useAuth();
     const navigate = useNavigate();
 
@@ -16,34 +12,25 @@ export function Home() : JSX.Element {
     useEffect(() => {getSession().then()}, []);
     console.log(session);
     return (
-        <>
+        <div id="login">
             {isLoggedIn()
                 ?  <LoggedIn session={session!} deleteUser={deleteUser} navigate={navigate} logout={logout} />
                 : <LoggedOut login={login}/>
             }
-        </>
+        </div>
     )
 
 }
 
-<<<<<<< HEAD
 
-function LoggedIn({logout, session, deleteUser, navigate} :
-{
-=======
 type LoginProps = {
->>>>>>> 13e1dc504d87b0fa4288ec041312f01c468236f7
     logout: () => void,
     session: User,
     deleteUser: () => void,
     navigate: (destination: string) => void,
-<<<<<<< HEAD
-}) : JSX.Element {
-=======
 }
 
 function LoggedIn({logout, session, deleteUser, navigate} : LoginProps) : JSX.Element {
->>>>>>> 13e1dc504d87b0fa4288ec041312f01c468236f7
     return (
         <>
             <button onClick={logout}>logout</button>
@@ -52,33 +39,25 @@ function LoggedIn({logout, session, deleteUser, navigate} : LoginProps) : JSX.El
 
             <button onClick={deleteUser}>Delete</button>
 
-            <Notes notes={[]} />
-<<<<<<< HEAD
+            {/* <Notes notes={[]} /> */}
             <button onClick={() => navigate("/dev/sensors")}> dev </button>
-=======
             <button onClick={() => navigate("/dev/sensors")}> wacky-silly </button>
->>>>>>> 13e1dc504d87b0fa4288ec041312f01c468236f7
         </>
     )
 }
 
-<<<<<<< HEAD
-function LoggedOut({login}: { login: () => void }) : JSX.Element {
-=======
 type LogoutProps = {
     login: () => void,
 }
 
 function LoggedOut({login}: LogoutProps) : JSX.Element {
->>>>>>> 13e1dc504d87b0fa4288ec041312f01c468236f7
     return (
-        <>
+        <div id="LoginButton">
             <button onClick={login}>login</button>
-        </>
+        </div>
     )
 }
 
-export default Home;
-=======
-}
->>>>>>> 13e1dc504d87b0fa4288ec041312f01c468236f7
+export default Home
+
+
