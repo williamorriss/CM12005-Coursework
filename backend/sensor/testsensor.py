8bec0aa26e40a9c81e7387fb49b15e3a7ce3b78e
maurice-borys <brillbusmorriss@gmail.com>
2026-04-24 04:22:06 +0100

    sensor rewrite

--- backend/sensor/testsensor.py
+++ backend/sensor/testsensor.py
@@ -1,94 +1,94 @@
+from asyncio.queues import Queue
 from datetime import datetime, timedelta
-from aiosqlite import Row
+from uuid import UUID, uuid4
 import numpy as np
 import asyncio
 import aiosqlite
-from sensor.sensor import Sensor, Sample
+from sensor.sensor import Sample
 from db import DBNAME
 from random import randrange
+
+type SensorHandle = UUID
+type ConnectionHandle = UUID
 
 def fake_sample() -> Sample:
     temperature = randrange(-10, 40)
     ph = randrange(0, 14)
     return Sample(temperature, ph, datetime.now())
 
-class TestSensor(Sensor):
-    def __init__(self, sensor_id: int, plant_id: int, name: str):
-        self._name = name
-        self._sensor_id = sensor_id
-        self._plant_id = plant_id
-        self._aggregate_delay = timedelta(seconds=3)
-        self._task: asyncio.Task[None] | None = None
-        self._watchers: list[asyncio.Queue[Sample]] = []
 
-    @staticmethod
-    def from_row(row: Row) -> "TestSensor":
-        return TestSensor(
-            sensor_id=row["SensorID"],
-            plant_id=row["Plant_id"],
-            name=row["Name"]
-        )
+def collate(samples: np.ndarray) -> Sample:
+    return Sample(
+        float(samples[:, 0].mean()),
+        float(samples[:, 1].mean()),
+        samples[:, 2].max()
+    )
+  
+class SensorSystem:
+    _instance: "SensorSystem | None" = None
 
-    def start(self) -> None:
-        print(f"{self._name} {self._sensor_id} RUNNING")
-        self._task = asyncio.create_task(self._process())
+    def __new__(cls) -> "SensorSystem":
+        if cls._instance is None:
+            cls._instance = (super().__new__(cls))
 
-    def stop(self) -> None:
-        if self._task:
-            self._task.cancel()
-            self._task = None
+        assert cls._instance is not None
+        return cls._instance
 
-    def is_running(self) -> bool:
-        return self._task is not None and not self._task.done()
+    def __init__(self) -> None:
+        if hasattr(self, "connections"):  # already initialised
+            return
+        self._connections: dict[int, list[UUID]] = {}
+        self._listeners: dict[UUID, Queue[Sample]] = {}
 
-    def set_target(self, plant_id: int) -> None:
-        self._plant_id = plant_id
+    
+    async def _process(self):
+        while True:
+            collated = [
+                (x[0], collate(np.array(x[1])))
+                 for x in (await asyncio.gather(*[SensorSystem.collect_n(sensor_id) for sensor_id in self._connections.keys()])) 
+                 if x is not None
+            ]
+            asyncio.create_task(self._db_writer(collated))
+            for sensor_id, sample in collated:
+                for uuid in self._connections[sensor_id]:
+                    self._listeners[uuid].put_nowait(sample)
 
-    def add_watcher(self, queue: asyncio.Queue[Sample]) -> None:
-        self._watchers.append(queue)
-
-    def remove_watcher(self, queue: asyncio.Queue[Sample]) -> None:
-        self._watchers.remove(queue)
-
-    async def _process(self) -> None:
-        samples: list[Sample] = []
-        next_write = datetime.now() + self._aggregate_delay
-
-        try:
-            while True:
-                if datetime.now() > next_write:
-                    if samples:
-                        collated = TestSensor._collate(np.array(samples))
-                        await self._db_writer(collated)
-                        await self._push_collated(collated)
-
-                    samples.clear()
-                    next_write = datetime.now() + self._aggregate_delay
-
-                samples.append(fake_sample())
-                await asyncio.sleep(1)
-        except asyncio.CancelledError:
-            pass
-
-    async def _push_collated(self, collated: Sample) -> None:
-        for listener in self._watchers:
-            await listener.put(collated)
-
-    @staticmethod
-    def _collate(samples: np.ndarray) -> Sample:
-        return Sample(
-            float(samples[:, 0].mean()),
-            float(samples[:, 1].mean()),
-            samples[:, 2].max()
-        )
-
-    async def _db_writer(self, collated: Sample) -> None:
-        print(f"WRITING {collated}")
-
+    async def _db_writer(self, collated: list[tuple[int,Sample]]) -> None:
         async with aiosqlite.connect(DBNAME) as db:
-            temp, ph, timestamp = collated
-            await db.execute(
-                "INSERT INTO Logs (SensorID, PlantID, temperature, pH, CollectedTimestamp) VALUES (?, ?, ?, ?, ?)",
-                (self._sensor_id, self._plant_id, temp, ph, timestamp)
+            await db.executemany(
+                "INSERT INTO Logs (SensorID, temperature, pH, CollectedTimestamp) VALUES (?, ?, ?, ?, ?)",
+                [(sensor_id, sample.temperature, sample.ph, sample.timestamp) for sensor_id, sample in collated]
             )
+
             await db.commit()
+
+            
+    @staticmethod
+    async def collect_n(sensor_id: int, n: int = 6, delay_seconds: float = 10) -> tuple[int,list[Sample]] | None:
+        samples = []
+        try:
+            for _ in range(n):
+                sample = fake_sample()
+                samples.append(sample) 
+                await asyncio.sleep(delay_seconds) 
+            return sensor_id,samples
+        except asyncio.CancelledError:
+            return None 
+
+
+
+    def create_listener(self, sensor_id: int, plant_id: int) -> tuple[UUID, Queue[Sample]]:
+        queue: Queue[Sample] = Queue()
+        uuid = uuid4()
+        self._listeners[uuid] = queue
+        self._connections.setdefault(sensor_id, []).append(uuid)
+        return uuid, queue
+
+    def remove_listener(self, sensor_id: int, queue_id: UUID) -> None:
+        if sensor_id in self._connections:
+            self._connections[user_id].remove(queue_id)
+            del self._listeners[queue_id]
+
+
+
+
